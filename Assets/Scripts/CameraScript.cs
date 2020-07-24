32a6353ec1117e79e7ce88d53f7653bb402c8a71
Patak007 <tonio.ercegovic4@gmail.com>
2020-07-24 18:12:05 +0200

    player, camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public float playerOffset;
+
+    string playerColor;
+
+    
+    void Update()
+    {
+        if(playerTransform != null && playerTransform.position.y + playerOffset > transform.position.y){
+            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y + playerOffset, -10f);
+        }
+    }
+
+}
