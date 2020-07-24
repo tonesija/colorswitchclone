973c1193216ffc4b6699f2b7c748973f721c26f9
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-07-24 18:12:31 +0200

    obstacle prefabs, star and color changer sprites

---
+++ Assets/Scripts/LineMoveScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineMoveScript : MonoBehaviour
+{
+    public float linearSpeed;
+    private float cameraWidth;
+
+    void Start()
+    {
+        Camera orthoCam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        cameraWidth = 2.0f * orthoCam.orthographicSize * (float) Screen.width / (float) Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-1.0f * linearSpeed * Time.deltaTime, 0.0f, 0.0f);
+        
+        if(Mathf.Abs(transform.position.x) > 4 * 3.4f * transform.localScale.x){
+            transform.position = new Vector3(0.0f, transform.position.y, 0.0f);
+        }
+    }
+}
