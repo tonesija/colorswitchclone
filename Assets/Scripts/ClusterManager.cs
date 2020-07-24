1b172c6d703a6ba72cf7ae6005fc03ca7f131506
Patak007 <tonio.ercegovic4@gmail.com>
2020-07-24 22:11:08 +0200

    cluster manager and game manager

---
+++ Assets/Scripts/ClusterManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterManager : MonoBehaviour
+{
+    public GameObject[] origClusters;
+
+    //4 difficulties [1, 4]
+    public GameObject SpawnCluster(int difficulty, float spawnLoc){
+        int i = Random.Range(0, difficulty*origClusters.Length/4);
+        GameObject cluster = Instantiate(origClusters[i] , new Vector2(0f, spawnLoc), Quaternion.identity);
+        return cluster;
+    }
+}
