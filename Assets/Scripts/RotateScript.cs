78bf3eaa4499cf524f9d01a3603af0c1d1be10b4
Patak007 <tonio.ercegovic4@gmail.com>
2020-07-24 16:37:12 +0200

    rotate script

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime, Space.Self);
+    }
+}
