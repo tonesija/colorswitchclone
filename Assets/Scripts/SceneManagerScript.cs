7b78e589ff03ec6c1284ad7ab085ad6196c72f7e
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-07-26 14:44:16 +0200

    menu scene

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadGameScene(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadMenuScene(){
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void LoadAboutScene(){
+        SceneManager.LoadScene("AboutScene");
+    }
+}
